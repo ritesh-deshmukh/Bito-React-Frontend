38273b86fc7b29cb125bcef1703f7a35984a75e8
ritesh-deshmukh <riteshdeshmukh260@gmail.com>
2018-09-23 16:14:50 -0500

    auto render component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
 import Chart1 from './components/Chart1';
 import Dimensions from 'react-dimensions';
 import './App.css';
@@ -25,35 +26,42 @@ class App extends Component {
     };
   }  
 
-  componentDidMount() {
+  async componentDidMount() {
     // fetch("http://127.0.0.1:5000/insert_rand")
-    fetch("http://127.0.0.1:5001/next10")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            data: [
-              { label: result[0][1], value: result[0][3] },
-              { label: result[1][1], value: result[1][3] },
-              { label: result[2][1], value: result[2][3] },
-              { label: result[3][1], value: result[3][3] },
-              { label: result[4][1], value: result[4][3] },
-              { label: result[5][1], value: result[5][3] },
-              { label: result[6][1], value: result[6][3] },
-              { label: result[7][1], value: result[7][3] },
-              { label: result[8][1], value: result[8][3] },
-              { label: result[9][1], value: result[9][3] },
-            ]
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+    try{
+      setInterval(async () => {
+        // fetch("http://127.0.0.1:5000/insert_rand")
+        fetch("http://127.0.0.1:5001/next10")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              data: [
+                { label: result[0][1], value: result[0][3] },
+                { label: result[1][1], value: result[1][3] },
+                { label: result[2][1], value: result[2][3] },
+                { label: result[3][1], value: result[3][3] },
+                { label: result[4][1], value: result[4][3] },
+                { label: result[5][1], value: result[5][3] },
+                { label: result[6][1], value: result[6][3] },
+                { label: result[7][1], value: result[7][3] },
+                { label: result[8][1], value: result[8][3] },
+                { label: result[9][1], value: result[9][3] },
+              ]
+            });
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+      },5000)
+    } catch(e){
+      console.log(e)
+    }
   }
 
   componentWillUnmount() {
@@ -72,6 +80,7 @@ class App extends Component {
     } else {
         return (
         <div className="App">
+          <Header />
           <Chart1 
             data={data}
             width={containerWidth}
