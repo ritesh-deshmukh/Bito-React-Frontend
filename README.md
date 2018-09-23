9131396fc9e96f0ee5a07107248b3d77f188c645
ritesh-deshmukh <riteshdeshmukh260@gmail.com>
2018-09-23 12:39:58 -0500

    API json response check

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-﻿
+﻿https://www.npmjs.com/package/d3-scale
+npm install d3-scale
+
+https://www.npmjs.com/package/d3-shape
+npm install d3-shape
--- src/App.js
+++ src/App.js
@@ -3,19 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:5001/next10")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        }
+      )
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    const { error, isLoaded, items } = this.state;
+    console.log(this.state.items)
+      return (
+        <div></div>
+      );
+    
   }
 }
-
 export default App;
